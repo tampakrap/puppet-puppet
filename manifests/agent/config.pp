7441fe6083dcf20532b6dc258bc75651bc570cfa
Theo Chatzimichos <tampakrap@gmail.com>
2015-02-13 12:15:43 +0100

    cosmetics: combine all ini_settings

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -2,13 +2,8 @@ class puppet::agent::config {
 
   Ini_setting {
     path    => $puppet::conf,
+    section => 'agent',
     ensure  => 'present',
-  }
-
-  ini_setting { 'server':
-    section => 'main',
-    setting => 'server',
-    value   => $puppet::agent::server,
   }
 
   if $puppet::agent::ca_server {
@@ -17,75 +12,51 @@ class puppet::agent::config {
     $real_ca_server = $puppet::agent::server
   }
 
-  ini_setting { 'ca_server':
-    section => 'main',
-    setting => 'ca_server',
-    value   => $real_ca_server,
-  }
-
   if $puppet::agent::report_server {
     $real_report_server = $puppet::agent::report_server
   } else {
     $real_report_server = $puppet::agent::server
   }
 
-  ini_setting { 'report_server':
-    section => 'main',
-    setting => 'report_server',
-    value   => $real_report_server,
-  }
-
-  ini_setting { 'pluginsync':
-    section => 'agent',
-    setting => 'pluginsync',
-    value   => $puppet::agent::pluginsync,
-  }
-
-  ini_setting { 'certname':
-    section => 'agent',
-    setting => 'certname',
-    value   => $puppet::agent::certname,
-  }
-
-  ini_setting { 'report':
-    section => 'agent',
-    setting => 'report',
-    value   => $puppet::agent::report,
-  }
-
-  ini_setting { 'environment':
-    section => 'agent',
-    setting => 'environment',
-    value   => $puppet::agent::environment,
-  }
-
-  ini_setting { 'show_diff':
-    section => 'agent',
-    setting => 'show_diff',
-    value   => $puppet::agent::showdiff,
-  }
-
-  ini_setting { 'splay':
-    section => 'agent',
-    setting => 'splay',
-    value   => $puppet::agent::splay,
-  }
-
-  ini_setting { 'configtimeout':
-    section => 'agent',
-    setting => 'configtimeout',
-    value   => $puppet::agent::configtimeout,
-  }
-
-  ini_setting { 'usecacheonfailure':
-    section => 'agent',
-    setting => 'usecacheonfailure',
-    value   => $puppet::agent::usecacheonfailure,
-  }
-
-  ini_setting { 'stringify_facts_agent':
-    setting => 'stringify_facts',
-    section => 'agent',
-    value   => $puppet::agent::stringify_facts;
+  ini_setting {
+    'server':
+      section => 'main',
+      setting => 'server',
+      value   => $puppet::agent::server;
+    'ca_server':
+      section => 'main',
+      setting => 'ca_server',
+      value   => $real_ca_server;
+    'report_server':
+      section => 'main',
+      setting => 'report_server',
+      value   => $real_report_server;
+    'pluginsync':
+      setting => 'pluginsync',
+      value   => $puppet::agent::pluginsync;
+    'certname':
+      setting => 'certname',
+      value   => $puppet::agent::certname;
+    'report':
+      setting => 'report',
+      value   => $puppet::agent::report;
+    'environment':
+      setting => 'environment',
+      value   => $puppet::agent::environment;
+    'show_diff':
+      setting => 'show_diff',
+      value   => $puppet::agent::showdiff;
+    'splay':
+      setting => 'splay',
+      value   => $puppet::agent::splay;
+    'configtimeout':
+      setting => 'configtimeout',
+      value   => $puppet::agent::configtimeout;
+    'usecacheonfailure':
+      setting => 'usecacheonfailure',
+      value   => $puppet::agent::usecacheonfailure;
+    'stringify_facts_agent':
+      setting => 'stringify_facts',
+      value   => $puppet::agent::stringify_facts;
   }
 }
